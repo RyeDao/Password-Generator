471d8eb411bf6eeec37d1280b96becb155a9be93
RyeDao <ryan.t.dao@gmail.com>
2022-09-06 20:43:28 -0700

    Added functions to jss and updated README

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Password Generator Starter Code
+
+A random password generator that includes: special characters, numerical characters, lower cased characters and upper cased characters.
+
+A minimum of 8 characters to a maximum of 128 characters are accepted values to generate a password.
+
+Any amount under or over the alloted limit will not be able to generate.
+
+Referred to https://www.net-comber.com/charset.html for characters.
--- script.js
+++ script.js
@@ -17,13 +17,68 @@ const randomFunc = {
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function generatePassword(lower, upper, number, symbol, length) {
+	let generatedPassword = '';
+	const typesCount = lower + upper + number + symbol;
+	const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
+	
+	if(typesCount === 0) {
+		return '';
+	}
 
-  passwordText.value = password;
-
+	
+	for(let i=0; i<length; i+=typesCount) {
+		typesArr.forEach(type => {
+			const funcName = Object.keys(type)[0];
+			generatedPassword += randomFunc[funcName]();
+		});
+	}
+	
+	const finalPassword = generatedPassword.slice(0, length);
+	
+	return finalPassword;
 }
 
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generate.addEventListener('click', () => {
+	const length = +lengthEl.value;
+  if(Number.isNaN(length)){
+    alert("Password Length must be provided as a number")
+    return null;
+  }
+
+  if(length < 8){
+    alert("Password must be at least 8 characters long!")
+    return null;
+  }
+
+  if(length > 128){
+    alert("Password cannot exceed 128 characters!")
+    return null;
+  }
+	const hasLower = lowercaseEl.checked;
+	const hasUpper = uppercaseEl.checked;
+	const hasNumber = numbersEl.checked;
+	const hasSymbol = symbolsEl.checked;
+	
+	passwordEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+});
+
+//used Charset for numbers and characters
+function getRandomLower() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+	return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+	const symbols = '!@#$%^&*(){}[]=<>/,.'
+	return symbols[Math.floor(Math.random() * symbols.length)];
+}
